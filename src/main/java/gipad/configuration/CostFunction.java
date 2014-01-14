07c2128e8b411e46bfa6e257bf847c52c3585406
THOMAS POCREAU <thomas.poc@gmail.com>
2014-01-14 11:49:03 +0100

    Creation du package gipad.configuration.configuration déplacement de CostFunction

---
+++ src/main/java/gipad/configuration/CostFunction.java
@@ -0,0 +1,13 @@
+package gipad.configuration;
+
+import entropy.plan.durationEvaluator.DurationEvaluator;
+
+/**Interface of the Cost Function for the scheduler
+ * We have to create a function able to balance between many dimension
+ * See the "Fil Rouge"
+ * @author Pocman
+ *
+ */
+public interface CostFunction extends DurationEvaluator {
+
+}
