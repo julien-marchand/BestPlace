8b51f8c86b431571795a0c5246907752ee38babb
Clément Guillaume <clementguillaume56@gmail.com>
2014-02-10 15:21:51 +0100

    Data calcl prototype

--- src/main/java/gipad/tools/DataCalculateur.java
+++ src/main/java/gipad/tools/DataCalculateur.java
@@ -1,6 +1,7 @@
 package gipad.tools;
 
 import org.discovery.DiscoveryModel.model.Cpu;
+import org.discovery.DiscoveryModel.model.HardwareSpecification;
 import org.discovery.DiscoveryModel.model.Node;
 
 public class DataCalculateur {
@@ -10,5 +11,23 @@ public class DataCalculateur {
 			sum += cpu.nbCores() * cpu.getCpuCapacity();
 		}
 		return sum;
+	}
+
+	public static double getSumCpuCurrentUsage(
+			HardwareSpecification hardwareSpecification) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static double getSumOutUsage(
+			HardwareSpecification hardwareSpecification) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static double getSumCpuUsage(
+			HardwareSpecification hardwareSpecification) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
